2cdf67b962eb86c48fae482d1f653e0039676e08
Lara-Jalloul <laraadjalloul@gmail.com>
2022-01-05 01:54:43 +0200

    update tool box

--- front/src/components/Tool/Tool.js
+++ front/src/components/Tool/Tool.js
@@ -1,10 +1,8 @@
 import React from "react";
 import thumb from "../../assets/thumb-2 (1).png";
 import "./Tool.css";
-import { BsBellFill } from "react-icons/bs";
-import { FaEnvelopeOpen } from "react-icons/fa";
-import { FaVideo } from "react-icons/fa";
-import check from "../../assets/check.svg";
+import Info from "../Tool/Info/Info";
+import Icons from "../Tool/Icons/Icons";
 
 function Tool() {
   return (
@@ -13,57 +11,10 @@ function Tool() {
         <div className="tool-left">
           <img src={thumb} alt="" />
         </div>
-
         <div className="tool-right">
-          <span className="tool-title">Work faster with powerful tools.</span>
-          <ul>
-            <li>
-              <img src={check} className="check" alt="" />
-              <p>
-                Combined with a handful of model sentence structures looks
-                reasonable.
-              </p>
-            </li>
-            <li>
-              <img src={check} className="check" alt="" />
-              <p>
-                Contrary to popular belief, Lorem ipsum is not simply
-                randomtext.
-              </p>
-            </li>
-            <li>
-              <img src={check} className="check" alt="" />
-              <p>
-                Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-                accusantium.
-              </p>
-            </li>
-
-            <li>
-              <img src={check} className="check" alt="" />
-              <p>
-                Natus error sit voluptatem unde omnis iste natus error sit
-                volupatatem accusantium.
-              </p>
-            </li>
-            <li>
-              <img src={check} className="check" alt="" />
-              <p>
-                All the Lorem Ipsum generators on the Internet tend to repeat
-                necessary.
-              </p>
-            </li>
-          </ul>
+          <Info />
           <div className="container-icons">
-            <div className="icon-tool">
-              <BsBellFill />
-            </div>
-            <div className="icon-tool">
-              <FaEnvelopeOpen />
-            </div>
-            <div className="icon-tool">
-              <FaVideo />
-            </div>
+            <Icons />
           </div>
         </div>
       </div>
