b80f176f66bf0403144678b6d46bcf53c99dc6e6
Lara-Jalloul <laraadjalloul@gmail.com>
2022-01-06 16:49:57 +0200

    fixing

--- front/src/components/GetStarted/GetStarted.js
+++ front/src/components/GetStarted/GetStarted.js
@@ -1,25 +1,23 @@
 import React from "react";
 import "./GetStarted.css";
-import mobile from '../../assets/thumb-1.png';
+import mobile from "../../assets/thumb-1.png";
 
 function GetStarted() {
   return (
-    <div>
-      <div className="polygon"></div>
-      <div className="container">
-        <div className="left">
-          <div className="showcase">Creative way to Showcase your App</div>
-          <div className="parag">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
-          </div>
-          <button className="btn">Get Started</button>
+    <div className="container-start">
+      <div className="left">
+        <div className="polygon"></div>
+        <div className="showcase">Creative way to Showcase your App</div>
+        <div className="parag">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
         </div>
-        <div className="right">
-            <img src={mobile} alt=""/>
-        </div>
+        <button className="btn">Get Started</button>
+      </div>
+      <div className="right">
+        <img src={mobile} alt="" />
       </div>
     </div>
   );
