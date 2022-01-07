d524b18cf8c2f7e25c17a41e882c8b60504304d3
Lara-Jalloul <laraadjalloul@gmail.com>
2022-01-07 05:22:24 +0200

    all done

--- front/src/components/Navbar/Navbar.js
+++ front/src/components/Navbar/Navbar.js
@@ -4,35 +4,22 @@ import "./Navbar.css";
 function Navbar() {
   const [isMobile, setIsMobile] = useState(false);
   return (
-
-      <nav className="navbar">
-        <ul
-          className={isMobile ? "nav-menu active" : "nav-menu"}
-          onClick={() => setIsMobile(false)}
-        >
-          <li className="list drop">Home</li>
-          <li className="list">Features</li>
-          <li className="list drop">Pages</li>
-          <li className="list">Screenshots</li>
-          <li className="list">Pricing</li>
-          <li className="list">Contact</li>
-
-          {/* <button
-            className="mobile-menu-icon"
-            onClick={() => setIsMobile(!isMobile)}
-          >
-            {isMobile ? (
-              <i className="fas fa-times"></i>
-            ) : (
-              <i className="fas fa-bars"></i>
-            )}
-          </button> */}
-        </ul>
-        <div className="mobile-menu-icon" onClick={()=> setIsMobile(!isMobile)}>
-            <i className={isMobile ? "fas fa-times" : "fas fa-bars"}></i>
-          </div>
-      </nav>
- 
+    <nav className="navbar">
+      <ul
+        className={isMobile ? "nav-menu active" : "nav-menu"}
+        onClick={() => setIsMobile(false)}
+      >
+        <li className="list drop">Home</li>
+        <li className="list">Features</li>
+        <li className="list drop">Pages</li>
+        <li className="list">Screenshots</li>
+        <li className="list">Pricing</li>
+        <li className="list">Contact</li>
+      </ul>
+      <div className="mobile-menu-icon" onClick={() => setIsMobile(!isMobile)}>
+        <i className={isMobile ? "fas fa-times" : "fas fa-bars"}></i>
+      </div>
+    </nav>
   );
 }
 
